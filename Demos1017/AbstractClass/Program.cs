1a4a31cfeb384a2a780e46217ea950ba399531d1
a.khan <a.khan4@students.clark.edu>
2019-10-17 14:44:15 -0700

    thursday class demo 1017

---
+++ Demos1017/AbstractClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AbstractBaseClass abc = new AbstractBaseClass();  //compiler will not allow that
+
+            ChildClass cc = new ChildClass();
+            Console.WriteLine(cc.AbstractMethod(123));
+        }
+    }
+}
---
+++ Demos1017/Array of Objects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_of_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            double d = 456.789;
+            string s = "some string";
+            MyClass mc = new MyClass();
+
+
+
+            
+        }
+    }
+}
